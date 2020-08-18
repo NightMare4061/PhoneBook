56875f1be8b713e9fa9d687fc24637f9a14bb098
Rushan <2006r2017r@gmail.com>
2020-08-18 12:13:54 +0300

    Добавление модуля

--- PhoneBook.py
+++ PhoneBook.py
@@ -7,30 +7,13 @@
 # address - адрес, строка
 # {tel: [last_name, first_name, patronymic, address]}
 
-def input_data():
-    temp = list()
-    first_name = input("Введите имя: ")
-    last_name = input("Введите фамилию: ")
-    patronymic = input("Введите отчество: ")
-    address = input("Введите адрес: ")
-    temp.append(last_name)
-    temp.append(first_name)
-    temp.append(patronymic)
-    temp.append(address)
-    return temp
-
+from PhoneBook_lib import *
 
 phone_book = dict()
 
-print("*" * 41)
-print("*** PhoneBook - телефонный справочник ***")
-print("*" * 41)
-print("Режим работы:")
-print("1. Показать все записи")
-print("2. Добавить запись")
-print("3. Редактировать запись")
-print("4. Удалить запись")
-print("0. Выход")
+welcome()
+
+menu()
 
 choice = int(input("Введите режим работы"))
 
